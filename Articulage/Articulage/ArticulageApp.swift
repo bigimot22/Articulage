8af23732c6bc9d2c6644f74c3da7f8dca5e735c9
Johandre Delgado <bigimot22@gmail.com>
2022-03-28 10:43:18 +0300

    :tada: Initial commit

---
+++ Articulage/Articulage/ArticulageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticulageApp.swift
+//  Articulage
+//
+//  Created by Johandre Delgado on 28.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ArticulageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Articulage/Articulage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Articulage
+//
+//  Created by Johandre Delgado on 28.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
